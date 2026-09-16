d498cfe6fd8c763f9555568cadd691790c80d1d2
brandall <brandall@face-unt.ar>
2026-09-16 10:07:19 -0300

    fix: pass schema option to PrismaPg (Prisma 7 ignores ?schema= in URL at runtime)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,9 @@ import { PrismaPg } from '@prisma/adapter-pg'
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+  const connectionString = process.env.DATABASE_URL!
+  const schema = new URL(connectionString).searchParams.get('schema') ?? undefined
+  const adapter = new PrismaPg({ connectionString }, { schema })
   return new PrismaClient({ adapter })
 }
 
